a8128f81563ccbf29a64e4813abe870c1c652396
Harabiih <hauwahrabii@gmail.com>
2025-03-24 15:47:29 +0100

    Users table now properly set up for device

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_03_24_135738) do
+ActiveRecord::Schema[8.0].define(version: 2025_03_24_144425) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -44,7 +44,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_03_24_135738) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
-    t.string "password_digest"
     t.string "role"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
